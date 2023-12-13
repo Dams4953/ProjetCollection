86ca69e01b5165f371ed82e58c70f3b6674ac4c8
Damien <legat@CQJ>
2023-12-13 11:22:51 +0100

    ajout de l'objet

---
+++ script.js
@@ -0,0 +1,142 @@
+let collection = [
+    {
+      name: "Spartacus",
+      director: "Steven S. DeKnight",
+      releaseYear: 2010,
+      picture: "images/spartacus.jpg",
+      genre: ["action", "drame"],
+      cast: [
+        "Andy Whitfield",
+        "Liam McIntyre",
+        "Lucy Lawless",
+        "John Hannah",
+      ],
+    },
+  
+    {
+        name: "viking",
+        director: "Keith Thompson",
+        releaseYear: 2013,
+        picture: "images/viking.jpg",
+        genre: ["historique", "drame"],
+        cast: [
+          "Travis Fimmel",
+          "Katheryn Winnick",
+          "Clive Standen",
+          "Jessalyn Gilsig",
+        ],
+      },
+
+      {
+        name: "breaking bad",
+        director: "Mark Johnson",
+        releaseYear: 2008,
+        picture: "images/breakingBad.jpg",
+        genre: ["policier", "drame"],
+        cast: [
+          "Bryan Cranston",
+          "Aaron Paul",
+          "Anna Gunn",
+          "Dean Norris",
+        ],
+      },
+
+      {
+        name: "the walking dead",
+        director: "Frank Darabont",
+        releaseYear: 2010,
+        picture: "images/TheWalkingDead.jpg",
+        genre: ["horreur", "drame"],
+        cast: [
+          "Andrew Lincoln",
+          "Norman Reedus",
+          "Melissa McBride",
+          "Lauren Cohan",
+        ],
+      },
+
+      {
+        name: "game of thrones",
+        director: "Mark Huffam",
+        releaseYear: 2011,
+        picture: "images/GameOfThrones.jpg",
+        genre: ["fantasy", "drame"],
+        cast: [
+          "Emilia Clarke",
+          "Kit Harington",
+          "Peter Dinklage",
+          "Sophie Turner",
+        ],
+      },
+
+      {
+        name: "band of brothers",
+        director: "Steven Spielberg",
+        releaseYear: 2001,
+        picture: "BandOfBrothers.jpg",
+        genre: ["drame", "guerre"],
+        cast: [
+          "principaux 	Damian Lewis",
+          "Ron Livingston",
+          "Scott Grimes",
+          "Donnie Wahlberg",
+        ],
+      },
+
+      {
+        name: "the 100",
+        director: "Tim Scanlan",
+        releaseYear: 2014,
+        picture: "images/The100.jpg",
+        genre: ["science-fiction", "drame"],
+        cast: [
+          "Bob Morley",
+          "Eliza Taylor-Cotter",
+          "Lindsey Morgan",
+          "Marie Avgeropoulos",
+        ],
+      },
+
+      {
+        name: "kaamelott",
+        director: "Alexandre Astier ",
+        releaseYear: 2005,
+        picture: "images/Kaamelott.jpg",
+        genre: ["drame", "humoristique"],
+        cast: [
+          "Alexandre Astier",
+          "Lionnel Astier",
+          "Franck Pitiot",
+          "Anne Girouard",
+        ],
+      },
+
+      {
+        name: "Black Mirror",
+        director: "Charlie Brooker",
+        releaseYear: 2011,
+        picture: "images/BlackMirror.jpg",
+        genre: ["Horreur", "Science-fiction"],
+        cast: [
+          "Bryce Dallas Howard",
+          "Anthony Mackie",
+          "Hayley Atwell",
+          "Jon Hamm",
+        ],
+      },
+
+      {
+        name: "south park",
+        director: "Trey Parker",
+        releaseYear: 1997,
+        picture: "images/SouthPark.jpg",
+        genre: ["animation", "humoristique"],
+        cast: [
+          "Trey Parker",
+          "Matt Stone",
+          "April Stewart",
+          "Mary Kay Bergman",
+        ],
+      },
+    
+  ];
